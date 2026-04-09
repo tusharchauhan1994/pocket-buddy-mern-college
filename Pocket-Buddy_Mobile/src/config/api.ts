6e663946ee8df49eaa74c014f60497b47f2437f6
Tushar Chauhan <121916751+tusharchauhan1994@users.noreply.github.com>
2026-04-09 16:35:47 +0530

    app run

--- Pocket-Buddy_Mobile/src/config/api.ts
+++ Pocket-Buddy_Mobile/src/config/api.ts
@@ -10,4 +10,4 @@
  */
 // For iPhone/Android physical device testing, use your computer's local IP
 // Make sure phone and computer are on the same WiFi network
-export const API_BASE_URL = "http://172.20.10.11:3000";
+export const API_BASE_URL = "http://192.168.1.8:3000";
